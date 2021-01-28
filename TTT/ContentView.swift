b8f8dbaa5ed53b432dd40a1101654dc4dd16f63f
a.m.student <iabgisbgs@gmail.com>
2021-01-28 10:16:20 -0600

    Initial Commit

---
+++ TTT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TTT
+//
+//  Created by A.M. Student on 1/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TTT/TTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTTApp.swift
+//  TTT
+//
+//  Created by A.M. Student on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
